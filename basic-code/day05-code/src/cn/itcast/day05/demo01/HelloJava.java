0bff324ff3b4e7000fd0d11f4acb542fbbfd85cd
nongsiwen <1134403991@qq.com>
2020-05-18 16:49:15 +0800

    修改hello Java类

---
+++ basic-code/day05-code/src/cn/itcast/day05/demo01/HelloJava.java
@@ -0,0 +1,12 @@
+package cn.itcast.day05.demo01;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+        System.out.println("welcome to git：" + getString());
+    }
+
+    private static String getString() {
+        return "张三";
+    }
+}
