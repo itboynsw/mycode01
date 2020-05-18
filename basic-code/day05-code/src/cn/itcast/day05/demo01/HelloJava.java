a3e1f6adfcb90baf7697d8a07ef76bc3b41bcf86
nongsiwen <1134403991@qq.com>
2020-05-18 17:14:34 +0800

    现在显示年龄

--- basic-code/day05-code/src/cn/itcast/day05/demo01/HelloJava.java
+++ basic-code/day05-code/src/cn/itcast/day05/demo01/HelloJava.java
@@ -3,10 +3,14 @@ package cn.itcast.day05.demo01;
 public class HelloJava {
     public static void main(String[] args) {
         System.out.println("hello java");
-        System.out.println("welcome to git：" + getString());
+        System.out.println("my name is " + getString() + ",I am "+ getAge()+ " years old");
     }
 
     private static String getString() {
         return "itBoys";
+    }
+
+    private static int getAge() {
+        return 18;
     }
 }
